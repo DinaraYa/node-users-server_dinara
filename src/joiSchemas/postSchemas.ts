da2acd28f95eb1162aff37cd733e840bde5e81ef
DinaraYa <tylemisovadinara@gmail.com>
2025-08-05 01:44:33 +0300

    hw_7 error express-validator was added

--- src/joiSchemas/postSchemas.ts
+++ src/joiSchemas/postSchemas.ts
@@ -1,4 +1,5 @@
 import Joi from 'joi';
+import {checkSchema, Location, Schema} from "express-validator";
 
 
 export const PostDtoSchema = Joi.object({
@@ -7,3 +8,65 @@ export const PostDtoSchema = Joi.object({
     title: Joi.string(),
     text: Joi.string()
 })
+
+
+export const postValidationSchema = {
+    id: {
+        notEmpty: {
+            errorMessage: 'ID is required'
+        },
+        isInt: {errorMessage: 'ID must be an integer'},
+        isLength: {
+            options: { min: 1, max: 4 },
+            errorMessage: 'Length must be between 1 and 4 digits'
+        },
+    } ,
+    userId: {
+        notEmpty: {
+            errorMessage: 'User ID is required'
+        },
+        isInt: {
+            errorMessage: 'User ID must be an integer'
+        },
+        isLength: {
+            options: { min: 1, max: 3 },
+            errorMessage: 'Length must be between 1 and 3 digits'
+        },
+    } ,
+    title: {
+        notEmpty: {
+            errorMessage: 'Title is required'
+        },
+        isLength: {
+            options: { min: 1, max: 50 },
+            errorMessage: 'Length must be between 1 and 50 characters'
+        },
+        isString: {
+            errorMessage: 'Text must be a string',
+        },
+    },
+    text: {
+        notEmpty: {
+            errorMessage: 'Text is required'
+        },
+        isLength: {
+            options: { min: 1, max: 1000 },
+            errorMessage: 'Length must be between 1 and 1000 characters'
+        },
+        isString: {
+            errorMessage: 'Text must be a string',
+        },
+    },
+}
+
+export const postValidationIdSchema = {
+    id: {
+        in: ['params'] as Location[],
+        notEmpty: { errorMessage: 'ID is required' },
+        isInt: { errorMessage: 'ID must be an integer' },
+        isLength: {
+            options: { min: 1, max: 4 },
+            errorMessage: 'Length must be between 1 and 4 digits'
+        },
+    }
+};
--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -4,8 +4,11 @@ import * as controller from '../controllers/PostController.ts'
 import asyncHandler from "express-async-handler";
 import {convertPostDto} from "../utils/tools.js";
 import {Post} from "../model/postTypes.js";
-import {PostDtoSchema} from "../joiSchemas/postSchemas.js";
+import {PostDtoSchema, postValidationIdSchema} from "../joiSchemas/postSchemas.js";
 import {HttpError} from "../errorHandler/HttpError.js";
+import { checkSchema, validationResult } from 'express-validator';
+import { postValidationSchema } from '../joiSchemas/postSchemas.js';
+
 
 
 export const postRouter = express.Router();
@@ -19,6 +22,16 @@ postRouter.use((req: Request, res: Response, next: NextFunction) => {
     next();
 })
 
+const validate = (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const message = errors.array()
+            .map(e => `${e.msg}`)
+            .join("");
+        throw new HttpError(400, message);
+    }
+    next();
+}
 
 
 
@@ -26,35 +39,44 @@ postRouter.get('/', asyncHandler(async (req:Request, res:Response) => {
         controller.getAllPosts(req, res);
 }))
 
-postRouter.get('/post/:id', asyncHandler(async (req:Request, res:Response) => {
-        const {id} = req.params;
-        if (!id) throw new HttpError(404,"Post not found");
+postRouter.get('/post/:id',
+    checkSchema(postValidationIdSchema),
+    validate,
+    asyncHandler(async (req:Request, res:Response) => {
+        // const {id} = req.params;
+        // if (!id) throw new HttpError(404,"Post not found");
         controller.getPostById(req, res)
 }))
 
-postRouter.post('/', asyncHandler(async (req:Request, res:Response):Promise<void>=> {
-    const postDto: Post = req.body;
-    const {error} = PostDtoSchema.validate(postDto);
-    if (error) throw new HttpError(400, error.message)
-    const post: Post| null = convertPostDto(postDto);
-    if(!post) throw new Error ("Bad request");
-    req.body = post as Post;
+postRouter.post('/', checkSchema(postValidationSchema),
+    validate,
+    asyncHandler(async (req:Request, res:Response):Promise<void>=> {
+    // const postDto: Post = req.body;
+    // const {error} = PostDtoSchema.validate(postDto);
+    // if (error) throw new HttpError(400, error.message)
+    // const post: Post| null = convertPostDto(postDto);
+    // if(!post) throw new Error ("Bad request");
+    // req.body = post as Post;
     controller.addPost(req, res);
 }))
 
-postRouter.put('/', asyncHandler(async (req: Request, res: Response):Promise<void> => {
-    const postDto: Post = req.body;
-    const {error} = PostDtoSchema.validate(postDto);
-    if (error) throw new HttpError(400, error.message)
-    const post: Post|null = convertPostDto(postDto);
-    if(!post) throw new Error ("Bad request");
-    req.body = post as Post;
+postRouter.put('/', checkSchema(postValidationSchema),
+    validate,
+    asyncHandler(async (req: Request, res: Response):Promise<void> => {
+    //const postDto: Post = req.body;
+   // const {error} = PostDtoSchema.validate(postDto);
+        // if (error) throw new HttpError(400, error.message)
+    // const post: Post|null = convertPostDto(postDto);
+    //if(!post) throw new Error ("Bad request");
+    //req.body = post as Post;
     controller.updatePost(req, res);
 }))
 
-postRouter.delete('/post/:id', asyncHandler(async (req:Request, res:Response):Promise<void> => {
-    const id = +req.params.id;
-    if (!id) throw new HttpError(404,"Post not found");
+postRouter.delete('/post/:id', checkSchema(postValidationIdSchema),
+    validate,
+    asyncHandler(async (req:Request, res:Response):Promise<void> => {
+     const id = +req.params.id
+    // if (!id) throw new HttpError(404,"Post not found");
     controller.removePost(req, res);
 }))
 
